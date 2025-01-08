a3acb376fda483d2ba60977f034c05cb140e4db0
Divyjain1919 <divyanshjain0986@gmail.com>
2025-01-08 10:26:46 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,23 @@
+
+// //!for loop
+//  for (let i = 0; i <=100; i++) {
+//     console.log(i);
+
+    
+//     }
+// for( let i=0 ; i<=1;i++){
+
+
+//     if(i%2!=0){
+//         console.log(i);
+//          console.log("is odd number")
+//          }
+//     else{
+//         console.log(i);
+//          console.log("is even number ")
+
+//          }
+//     }
+
+
+
