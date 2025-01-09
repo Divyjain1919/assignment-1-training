6c7145f9307c813fae1fb113b17d381ffa4880ba
Divyjain1919 <divyanshjain0986@gmail.com>
2025-01-09 11:49:52 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+
+// let x =_= >console.log("Arrow function");
+// x();
+
+// let x =a=>console.log(a);
+// x(5);
+
+// let x=(a,b)=>console.log(a+b);
+// x(5,5);
+// let x =_=>console.log("Hi");
+//     console.log("Hello");
+//     console.log("Bye");
+//     x();
---
+++ pattern.js
@@ -0,0 +1,27 @@
+// //star pattern in for loop 
+// for ($i=1;$i<=5;$i++){
+//         console.log("*");
+//         }
+//         console.log("");
+//         for ($i=1;$i<=4;$i++){
+//             console.log("* *");
+//             }
+//             console.log("");
+//             for ($i=1;$i<=3;$i++){
+//                 console.log("* * *");
+//                 }
+//                 console.log("");
+//                 for ($i=1;$i<=2;$i++){
+//                     console.log("* * * *");
+//                     }
+//                     console.log("");
+//                     for ($i=1;$i<=1;$i++){
+//                         console.log("* * * * *");
+//                         }
+//                         console.log("");
+
+
+
+
+ 
+                        
