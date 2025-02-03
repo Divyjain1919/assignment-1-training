b331438d6ed9a467c326b10e9b2fc9048610b995
Divyjain1919 <divyanshjain0986@gmail.com>
2025-02-03 12:51:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import logo from './spinner-removebg-preview.png';
+import './App.css';
+
+function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+let username = "SUBhamsss";
+return (
+  <div>
+  <h1>{username}</h1>
+<p>{100+100}</p>
+</div>
+);
+}
+
+export default App;
