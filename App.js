ad4522a61f49a8bba165c0a17800d2b8c37b1110
Divyjain1919 <divyanshjain0986@gmail.com>
2025-02-04 13:00:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import logo from './spinner-removebg-preview.png';
-import './App.css';
+// import logo from './spinner-removebg-preview.png';
+ import './App.css';
 
-function App() {
-//   return (
+// function App() {
+// //   return (
 //     <div className="App">
 //       <header className="App-header">
 //         <img src={logo} className="App-logo" alt="logo" />
@@ -21,13 +21,46 @@ function App() {
 //     </div>
 //   );
 // }
-let username = "SUBhamsss";
-return (
-  <div>
-  <h1>{username}</h1>
-<p>{100+100}</p>
-</div>
-);
-}
+// let username = "SUBhamsss";
+// return (
+//   <div>
+//   <h1>{username}</h1>
+// <p>{100+100}</p>
+// </div>
+// );
+// }
 
+// export default App;
+// import React, { Component } from 'react'
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>App</div>
+//     )
+//   }
+// }
+// ! component composition
+import React from 'react'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Sidebar1 from './components/Sidebar1'
+import Sidebar2 from './components/Sidebar2'
+import Footer from './components/Footer'
+const App = () => {
+
+return(
+<div className='app'>
+<Navbar/>
+<Main/>
+<div className='sidebar'>
+<Sidebar1/>
+<Sidebar2/>
+</div>
+<Footer/>
+</div>
+
+
+)
+}
 export default App;
---
+++ Sidebar2.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class sidebar2 extends Component {
+  render() {
+    return (
+      <div className='Sidebar2'>Sidebar2</div>
+    )
+  }
+}
